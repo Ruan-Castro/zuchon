7f09907837d48b85a6b59b180089661abd1efb39
Veganon <antoniomarcos.mc36@gmail.com>
2021-08-12 00:01:26 -0300

    Update

--- paginas/index.php
+++ paginas/index.php
@@ -19,7 +19,7 @@ if(!empty($_SESSION['ID'])){
 		}
 	?>
 	<header>
-		<button onclick="location.href='./novoticket.php'" class="bnt direita">Ticket +</button>
+		<button onclick="Modal.open()" class="bnt direita">Ticket +</button>
 		<button class="filtro corr"></button>
 		<button class="filtro cory"></button>
 		<button class="filtro corg"></button>
@@ -31,6 +31,47 @@ if(!empty($_SESSION['ID'])){
 	<footer>
 		
 	</footer>
+	<div class="modal-overlay">
+        <div class="modal">
+            <div id="form">
+                <h2>Novo Ticket</h2>
+                <form action="" onsubmit="Form.submit(event)">
+                    <div class="input-group">
+                        <label 
+                            class="sr-only" 
+                            for="resumo">Resumo</label>
+                        <input 
+                            type="text" 
+                            id="resumo" 
+                            name="resumo"
+                            placeholder="Resumo"
+                        />
+                    </div>
+
+                    <div class="input-group">
+                        <label 
+							class="sr-only" 
+							for="solicitacao">Solicitação</label>
+                        <input
+							type="text"
+							id="solicitacao" 
+							name="solicitacao"
+							placeholder="Sua solicitação"
+                        />
+                    </div>
+
+                    <div class="input-group actions">
+                        <a 
+                        onclick="Modal.close()"
+                        href="#" 
+                        class="button cancel">Cancelar</a>
+                        <button class="btn-cad">Salvar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+	<script src="../js/scripts.js"></script>
 </body>
 </html>
 <?php
