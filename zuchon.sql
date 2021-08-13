bd7bceaeddd8cf19f4b613ffca044af58bf73f39
Veganon <antoniomarcos.mc36@gmail.com>
2021-08-13 17:02:16 -0300

    Banco de Dados

--- zuchon.sql
+++ zuchon.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 06-Ago-2021 às 23:03
+-- Tempo de geração: 13-Ago-2021 às 22:01
 -- Versão do servidor: 10.4.20-MariaDB
 -- versão do PHP: 8.0.8
 
@@ -33,7 +33,7 @@ CREATE TABLE `chamado` (
   `SOLICITACAO` varchar(500) NOT NULL,
   `DTABERTURA` datetime NOT NULL,
   `DTENCERRAMENTO` datetime DEFAULT NULL,
-  `TECNICO` int(11) NOT NULL,
+  `TECNICO` int(11) DEFAULT NULL,
   `USUARIO` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -42,7 +42,11 @@ CREATE TABLE `chamado` (
 --
 
 INSERT INTO `chamado` (`ID`, `RESUMO`, `SOLICITACAO`, `DTABERTURA`, `DTENCERRAMENTO`, `TECNICO`, `USUARIO`) VALUES
-(1, 'TESTE', 'COMPRA DE CABO DE REDE DE TESTE', '2021-08-06 17:10:21', NULL, 1, 1);
+(1, 'TESTE', 'COMPRA DE CABO DE REDE DE TESTE', '2021-08-06 17:10:21', NULL, 1, 1),
+(2, '$resumo', '$solicitacao', '2021-08-12 12:08:09', '2021-08-12 12:09:38', 1, 0),
+(3, 'Oi', 'Teste', '2021-08-12 12:08:39', NULL, NULL, 2),
+(4, 'Hoje tem', 'Miau', '2021-08-12 12:30:51', NULL, NULL, 2),
+(5, 'Teste', 'ticket', '2021-08-12 13:53:02', NULL, NULL, 2);
 
 -- --------------------------------------------------------
 
@@ -78,7 +82,29 @@ CREATE TABLE `setor` (
 --
 
 INSERT INTO `setor` (`CODSETOR`, `NOMESETOR`) VALUES
-(1, 'T.I.');
+(1, 'T.I.'),
+(2, 'DIRETORIA'),
+(0, 'Comercial');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `sexo`
+--
+
+CREATE TABLE `sexo` (
+  `sexo` varchar(20) NOT NULL,
+  `codsexo` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `sexo`
+--
+
+INSERT INTO `sexo` (`sexo`, `codsexo`) VALUES
+('Masculino', 0),
+('Feminino', 1),
+('Outro', 2);
 
 -- --------------------------------------------------------
 
@@ -89,6 +115,7 @@ INSERT INTO `setor` (`CODSETOR`, `NOMESETOR`) VALUES
 CREATE TABLE `usu` (
   `ID` int(11) NOT NULL,
   `Nome` varchar(30) NOT NULL,
+  `sexo` int(1) NOT NULL,
   `usuario` varchar(20) NOT NULL,
   `senha` varchar(16) NOT NULL,
   `NA` int(2) NOT NULL,
@@ -99,8 +126,9 @@ CREATE TABLE `usu` (
 -- Extraindo dados da tabela `usu`
 --
 
-INSERT INTO `usu` (`ID`, `Nome`, `usuario`, `senha`, `NA`, `SETOR`) VALUES
-(1, 'Administrador', 'admin', 'administrador', 5, 1);
+INSERT INTO `usu` (`ID`, `Nome`, `sexo`, `usuario`, `senha`, `NA`, `SETOR`) VALUES
+(1, 'Lane Santos', 1, 'lane.santos', 'password', 5, 1),
+(2, 'Antonio Marcos', 0, 'marcos.castro', 'password', 5, 1);
 
 --
 -- Índices para tabelas despejadas
@@ -126,13 +154,13 @@ ALTER TABLE `usu`
 -- AUTO_INCREMENT de tabela `chamado`
 --
 ALTER TABLE `chamado`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de tabela `usu`
 --
 ALTER TABLE `usu`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
