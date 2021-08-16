dc960be1cde0130089dd7dc6e9145d44dbdf73ab
Veganon <antoniomarcos.mc36@gmail.com>
2021-08-16 13:36:49 -0300

    Tela de usuário concluída

--- paginas/index.php
+++ paginas/index.php
@@ -9,7 +9,7 @@ if(!empty($_SESSION['ID'])){
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	<title>Zuchon - HelpDesk</title>
 	<link rel="shortcut icon" href="../imagens/icon.ico" />
-	<link rel="stylesheet" type="text/css" href="../css/css2.css">
+	<link rel="stylesheet" type="text/css" href="../css/css.css">
 </head>
 <body>
 	<?php
@@ -20,9 +20,9 @@ if(!empty($_SESSION['ID'])){
 	?>
 	<header>
 		<a onclick="Modal.open()" class="bnt direita">Ticket +</a>
-		<button class="filtro corr"></button>
+		<!-- <button class="filtro corr"></button>
 		<button class="filtro cory"></button>
-		<button class="filtro corg"></button>
+		<button class="filtro corg"></button> -->
 		<a href="../php/sair.php" class="bnt esquerda"><?php echo $_SESSION['nome']; ?>
 		<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" color="red" class="bi bi-box-arrow-in-right" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z"/><path fill-rule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/></svg></a>
 	</header>
@@ -52,7 +52,7 @@ if(!empty($_SESSION['ID'])){
                     
                     //calcular o inicio visualização
                     $inicio = ($qnt_result_pg * $pagina) - $qnt_result_pg;
-                    
+                    $id_user=$_SESSION['ID'];
                     $result_usuarios = "
                     SELECT
                         A.ID,
@@ -68,13 +68,16 @@ if(!empty($_SESSION['ID'])){
                     INNER JOIN usu B ON A.USUARIO=B.ID
                     INNER JOIN Tecnicos C ON A.TECNICO=C.ID
                     INNER JOIN setor D ON B.setor=D.codsetor
+                    WHERE
+                        A.DTENCERRAMENTO IS NULL
+                        AND A.usuario='$id_user'
                     ";
                     $resultado_usuarios = mysqli_query($conn, $result_usuarios);
                     while($row_usuario = mysqli_fetch_assoc($resultado_usuarios)){
                         ?>
                         
                     <tr>
-                        <td class="<?php echo $row_usuario['PRIORIDADE'];?>"><?php echo $row_usuario['ID'];?></td>
+                        <td class="<?php echo $row_usuario['PRIORIDADE'];?>"><a href="" <?php if($row_usuario['PRIORIDADE']=="estrategico"){ echo "style='color: white;'";}else{ echo "style='color: black;'";} ?> ><?php echo $row_usuario['ID'];?></a></td>
                         <td><?php echo $row_usuario['NOME'];?></td>
                         <td><?php echo $row_usuario['NOMESETOR']; ?></td>
                         <td><?php echo $row_usuario['RESUMO'];?></td>
