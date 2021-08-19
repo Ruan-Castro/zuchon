5de97acb7fb233895c51dd462f8ff436483f5700
Veganon <antoniomarcos.mc36@gmail.com>
2021-08-19 17:26:16 -0300

    Adição do botão Pesquisa
    Botão pesquisa de chamados!

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ session_start();
 			    </div>
 			    <center>
 			    	<button type="submit" name="login">Entrar</button>
-						<div class="tecnico-usuario">Técnico? Clique <a href="./index2.php">aqui</a></div>
+					<div class="tecnico-usuario">Técnico? Clique <a href="./index2.php">aqui</a></div>
 					<div class="footer">
 						Criado por <a href="https://github.com/Ruan-Castro" target="_blank">Antonio Marcos</a>
 					</div>
--- paginas/index.php
+++ paginas/index.php
@@ -18,14 +18,20 @@ if(!empty($_SESSION['ID'])){
 			unset($_SESSION['msg']);
 		}
 	?>
-	<header>
-		<a onclick="Modal.open()" class="bnt direita">Ticket +</a>
-		<!-- <button class="filtro corr"></button>
-		<button class="filtro cory"></button>
-		<button class="filtro corg"></button> -->
-		<a href="../php/sair.php" class="bnt esquerda"><?php echo $_SESSION['nome']; ?>
-		<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" color="red" class="bi bi-box-arrow-in-right" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z"/><path fill-rule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/></svg></a>
-	</header>
+    <header>
+        <span>
+            <a onclick="Modal.open()" class="bnt direita">Ticket +</a>
+            <form action="pesquisa.php" method="POST">
+                <input type="text" name="chamado" placeholder="Pesquisar chamado" style="width: 150px; height: 10px; margin-right:0px;" required></input>
+                <button type="submit" name="Enviar" style="width: 30px; height: 30px;"><img src="../imagens/lupa.png"></button>        
+            </form>
+    		<!-- <button class="filtro corr"></button>
+    		<button class="filtro cory"></button>
+    		<button class="filtro corg"></button> -->
+    		<a href="../php/sair.php" class="bnt esquerda"><?php echo $_SESSION['nome']; ?>
+    		<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" color="red" class="bi bi-box-arrow-in-right" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z"/><path fill-rule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/></svg></a>
+    	</span>
+    </header>
 	<main class="container">
 		<section id="transaction">
             <table id="data-table">
